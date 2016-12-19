e1160751494d560b0218460f832aa8fb6c763a40
Jeff Stein <jstein@mitre.org>
2016-12-19 10:03:36 -0500

    Added exception case on missing info.plist directories

--- scan.py
+++ scan.py
@@ -11,16 +11,18 @@ def main(wf):
 	wf_directories = os.listdir(base_dir)
 
 	for wfd in wf_directories:
+		try:
+			wf_base_dir = base_dir + '/' + wfd + '/'
+			plist_file = wf_base_dir + 'info.plist'
+			pl = plistlib.readPlist(plist_file)
 
-		wf_base_dir = base_dir + '/' + wfd + '/'
-		plist_file = wf_base_dir + 'info.plist'
-		pl = plistlib.readPlist(plist_file)
+			# print pl.get('name')
+			# print 'Version: ' + pl.get('version','0')
+			icon_file = wf_base_dir + '/icon.png'
 
-		# print pl.get('name')
-		# print 'Version: ' + pl.get('version','0')
-		icon_file = wf_base_dir + '/icon.png'
-
-		wf.add_item(pl.get('name'), 'Version: ' + pl.get('version','0'), arg=wf_base_dir, type=u'file', icon=icon_file, valid=True)
+			wf.add_item(pl.get('name'), 'Version: ' + pl.get('version','0'), arg=wf_base_dir, type=u'file', icon=icon_file, valid=True)
+		except:
+			pass
 	wf.send_feedback()
 
 if __name__ == '__main__':
